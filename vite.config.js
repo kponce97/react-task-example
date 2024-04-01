ffcf61905b0c04e43e852302092312f294f55c78
Kevin Elias <kevin_elias_ponce@hotmail.com>
2024-04-01 14:43:25 -0300

    s5 commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-task-example/'
+  base: 'https://github.com/kponce97/react-task-example',
 })
